d88e404a9c28f8abfaeaef7341f1eb2ab1fd8457
aomnchp-s <natchaporn.aomnchp@gmail.com>
2024-06-27 22:24:14 +0700

    edit variable

--- Day3_20240622/35_vector.py
+++ Day3_20240622/35_vector.py
@@ -1,26 +1,10 @@
 class Vector:
     def __init__(self,v1:float, v2:float) -> None:
-        self.v1:float = v1
-        self.v2:float = v2
-
-    @property
-    def v1(self) -> float:
-        return self._v1
-    
-    @property
-    def v2(self) -> float:
-        return self._v2
-    
-    @v1.setter
-    def v1(self, v1:float) -> None:
-        self._v1:any = v1
-    
-    @v2.setter
-    def v2(self, v2:float) -> None:
-        self._v2:any = v2
+        self._v1:float = v1
+        self._v2:float = v2
 
     def __str__(self) -> str:
-        return f'Vector: {self.v1} {self.v2} \nMagnitude: {((self.v1)**2 + (self.v2)**2)**1/5}'
+        return f'Vector: {self._v1} {self._v2} \nMagnitude: {((self._v1)**2 + (self._v2)**2)**1/5}'
 
 
 def main() -> None:
